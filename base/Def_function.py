ae368672d7e71e6359159a2dfe6db10256b5516d
zither <325831871@qq.com>
2017-10-13 18:48:34 +0800

    add some demo

---
+++ base/Def_function.py
@@ -0,0 +1,4 @@
+def square_sum(a,b):
+    c = a**2 + b**2
+    return c,a,b
+print square_sum(3,1)
---
+++ base/For_Demo.py
@@ -0,0 +1,10 @@
+for a in [3,4.4,'life']:
+    print a
+
+Index = range(5)
+print Index
+
+i = 2
+while i < 10:
+    print i
+    i = i + 1
